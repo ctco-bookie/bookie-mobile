802844d62b0618011b661247f1af63ba9fbb9c07
jelisejev <pavel.yeliseev@ctco.lv>
2017-01-19 12:21:12 +0200

    connect room input

--- js/RoomPicker.js
+++ js/RoomPicker.js
@@ -3,6 +3,11 @@ import { View } from 'react-native';
 import { Text, Input, Content, Button, H1, Container, Header, Title, Icon, InputGroup } from 'native-base';
 
 export default class RoomPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {room: '408'};
+    }
+
     render() {
         return (
             <Container>
@@ -15,7 +20,7 @@ export default class RoomPicker extends Component {
                 <Content style={{paddingTop: 100}}>
                     <H1 style={{textAlign: 'center'}}>Pick a room</H1>
                     <InputGroup borderType='underline' style={{margin: 30}}>
-                        <Input placeholder="Room number" />
+                        <Input value={this.state.room} onChangeText={this.roomChange} />
                     </InputGroup>
                     <Button large onPress={this.navigate} style={{alignSelf: 'center'}}>Book now</Button>
                 </Content>
@@ -24,6 +29,12 @@ export default class RoomPicker extends Component {
     }
 
     navigate = () => {
-        this.props.navigator.push({id: 'roomList', roomNumber: '408'})
+        this.props.navigator.push({id: 'roomList', roomNumber: this.state.room})
+    }
+
+    roomChange = (room) => {
+        this.setState({
+            room
+        });
     }
 }
