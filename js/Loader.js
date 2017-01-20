7d3d1135577074d9f397d4d43edcd8fd1ab39656
Sergey Trasko <sergey.trasko@ctco.lv>
2017-01-20 12:33:48 +0200

    Changed loader color

--- js/Loader.js
+++ js/Loader.js
@@ -10,7 +10,7 @@ import {
 export default class Loader extends Component {
     render() {
         return <View style={{alignSelf: 'center'}}>
-            <Spinner style={{alignSelf: 'center'}} color="blue"/>
+            <Spinner style={{alignSelf: 'center'}} color="#5067FF"/>
             <Text>{this.props.text}</Text>
         </View>
     }
