bd7f5c03627eb75420bd444bbc6c29849f4e2656
jelisejev <pavel.yeliseev@ctco.lv>
2017-01-20 12:37:30 +0200

    room picker design

--- js/RoomPicker.js
+++ js/RoomPicker.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import {
     Input,
@@ -17,23 +18,24 @@ import {
 export default class RoomPicker extends Component {
     constructor(props) {
         super(props);
-        this.state = {room: '408'};
+        this.state = {room: null};
     }
 
     render() {
         return (
             <Container>
-                <Header>
-                    <Title>Bookie</Title>
-                </Header>
-                <Content style={{paddingTop: 100}}>
-                    <H1 style={{textAlign: 'center'}}>Pick a room</H1>
-                    <InputGroup borderType='underline' style={{margin: 30}}>
-                        <Input value={this.state.room} onChangeText={this.roomChange} keyboardType="numeric" />
-                    </InputGroup>
-                    <Button large onPress={this.navigate} style={{alignSelf: 'center'}}>Book now</Button>
-                    <Text>OR</Text>
-                    <Button large onPress={this.scan} style={{alignSelf: 'center'}}>Scan QR Code now</Button>
+                <Content style={{paddingTop: 50}} contentContainerStyle={{alignItems: 'center'}}>
+                    <Text style={{fontSize: 50, lineHeight: 50, padding: 50}}>Bookie</Text>
+                    <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                        <InputGroup borderType='underline' style={{margin: 30, marginRight: 10, marginLeft: 0, width: 180}}>
+                            <Input value={this.state.room} onChangeText={this.roomChange} keyboardType="numeric" style={{fontSize: 25, height: 60}} placeholder="Room number" />
+                        </InputGroup>
+                        <Button large onPress={this.navigate} style={{alignSelf: 'center'}}>Book Now</Button>
+                    </View>
+                    <Text style={{fontSize: 25, lineHeight: 25}}>OR</Text>
+                    <Button large onPress={this.scan} style={{alignSelf: 'center', marginTop: 20}}>
+                        <FontAwesomeIcon name="qrcode" style={{fontSize: 20}}></FontAwesomeIcon> Scan QR Code
+                    </Button>
                 </Content>
             </Container>
         );
